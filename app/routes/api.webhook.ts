49fe176d3856f08ef2cbaf00ddbda1c6557ee23b
kumait <Kumait@gmail.com>
2024-11-07 00:38:09 +0100

    Refactor webhook action function and update status handling
    Change the action function from a traditional function to an arrow function. Simplify status handling within json responses by passing status codes directly. Additionally, relocate the declaration of the 'signature' variable for clearer code structure.

--- app/routes/api.webhook.ts
+++ app/routes/api.webhook.ts
@@ -3,9 +3,11 @@ import {insertEvent} from "~/db/eventStorage.server";
 import {sanitizeEventData} from "~/utils/sanitizeData";
 import type {Event} from "~/types/type";
 
-export async function action({ request }: ActionFunctionArgs) {
+export const action = async ({
+                                 request,
+                             }: ActionFunctionArgs) => {
     if (request.method !== "POST") {
-        return json({ message: "Method not allowed" }, { status: 405 });
+        return json({ message: "Method not allowed" }, 405);
     }
 
     const crypto = await import("crypto");
@@ -14,21 +16,21 @@ export async function action({ request }: ActionFunctionArgs) {
     const deliveryId = request.headers.get("X-GitHub-Delivery") || "unknown";
 
     const webhookSecret = process.env.WEBHOOK_SECRET;
-    const signature = request.headers.get("X-Hub-Signature-256");
 
     if (!webhookSecret) {
         throw new Response("WEBHOOK_SECRET environment variable is not set.", {
             status: 500,
         });
     }
-
+    const signature = request.headers.get(
+        "X-Hub-Signature-256"
+    );
     const generatedSignature = `sha256=${crypto
-        .createHmac("sha256", webhookSecret)
-        .update(payloadText)
+        .createHmac("sha256",webhookSecret)
+        .update(JSON.stringify(payloadText))
         .digest("hex")}`;
-
     if (signature !== generatedSignature) {
-        return json({ message: "Signature mismatch" }, { status: 401 });
+        return json({ message: "Signature mismatch" }, 401);
     }
 
     const payload = await request.json();
@@ -52,6 +54,6 @@ export async function action({ request }: ActionFunctionArgs) {
     const success = await insertEvent(event);
 
     return success
-        ? json({ success: true, id: deliveryId }, { status: 201 })
+    ?  json({ success: true }, 200)
         : json({ message: "Failed to save event" }, { status: 500 });
 }
