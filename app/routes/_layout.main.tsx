5e8fbf5299ba437e2e73074f62d5eaef152f1595
kumait <Kumait@gmail.com>
2024-11-22 02:04:10 +0100

    moving the loader to the main (parent) route

--- app/routes/_layout.main.tsx
+++ app/routes/_layout.main.tsx
@@ -1,59 +1,71 @@
-
-
 import { LoaderFunction, ActionFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Outlet, useLoaderData } from "@remix-run/react";
+
 import INFO from "~/ui/ContactInfo";
 import { PhotoSection } from "./_layout.main.photos";
-import { loader as photosLoader } from "./_layout.main.photos";
-import { action as photoAction } from "./_layout.main.photos";
+import { loader as photosLoader, action as photoAction,  } from "./_layout.main.photos";
 
 import { CardsSection } from "./_layout.main.cards";
 import { loader as cardsLoader } from "./_layout.main.cards";
 
-import { TickerSection } from "./_layout.main.ticker";
-import { loader as tickerLoader } from "./_layout.main.ticker";
+import Ticker from "./_layout.main.ticker";
+import { CARDS } from "~/data";
 
 import { AppsSection } from "./_layout.main.apps";
-import { loader as appsLoader } from "./_layout.main.apps";
+import { App } from "~/types/type";
+import { connectDB } from "~/db/mongoDB.server";
+import invariant from "tiny-invariant";
 
 import Tabs from "~/ui/Tabs";
 import { loader as eventsLoader } from "./_events";
 
-
-
 export const loader: LoaderFunction = async (args) => {
-    const photoData = await photosLoader(args);
-    const cardsData = await cardsLoader(args);
-    const appsData = await appsLoader(args);
-    const tickerData = await tickerLoader(args);
-    const eventsData = await eventsLoader(args);
+    const { db } = await connectDB();
+    const data = await db.collection("app").findOne<{ apps: App[] }>({});
+    invariant(data, "No data found in the database");
 
+    const [cardsData, eventsData, photosData] = await Promise.all([
+        cardsLoader(args),
+        eventsLoader(args),
+        photosLoader(args),
+    ]);
 
-    return { photoData, cardsData, appsData, tickerData, eventsData };
+    return {
+        cardsData,
+        appsData: { apps: data.apps },
+        tickerData: { cards: CARDS },
+        eventsData,
+        photosData,
+    };
 };
 
 export const action: ActionFunction = photoAction;
 
 export default function MainLayout() {
-    const { photoData, cardsData, appsData, tickerData, eventsData } = useLoaderData<typeof loader>();
-    const { images, positions } = photoData;
-    const { eventTypes } = eventsData;
-
-
+    const { cardsData, appsData, tickerData, eventsData, photosData } = useLoaderData<typeof loader>();
 
     return (
         <div className="main-layout">
             <INFO />
-            <PhotoSection images={images} initialPositions={positions} />
+
+            <PhotoSection images={photosData.images} initialPositions={photosData.positions} />
+
             <CardsSection cards={cardsData.cards} />
+
             <AppsSection apps={appsData.apps} />
-            <nav className="container mx-auto p-8 flex justify-center bg-transparent rounded-lg overflow-hidden mt-12 z-10 relative"
+
+            <nav
+                className="container mx-auto p-8 flex justify-center bg-transparent rounded-lg overflow-hidden mt-12 z-10 relative"
                 style={{
                     boxShadow: "0 0 30px rgba(255, 255, 255, 0.8), 0 0 40px rgba(0, 0, 255, 0.6)",
                 }}
             >
-                <Tabs eventTypes={eventTypes} />
+                <Tabs eventTypes={eventsData.eventTypes} />
             </nav>
-            <TickerSection cards={tickerData.cards} />
-        </div>)
+
+            <Ticker cards={tickerData.cards} />
+
+            <Outlet />
+        </div>
+    );
 }
